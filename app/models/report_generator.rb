e187a2321dcbfacda3c3d004c7e7378fc1ad57e5
Austio@windowslaptop <lonnieastory@gmail.com>
2014-09-05 08:39:08 -0700

    Report generating proper json

--- app/models/report_generator.rb
+++ app/models/report_generator.rb
@@ -1,6 +1,6 @@
 class ReportGenerator
   def self.by_week
-    Expenses.all.group_by(&:week).inject({}) do |result, week|
+    Expense.all.group_by(&:week).inject({}) do |result, week|
       amount  = week.second.inject(0){ |r,expense| r + expense.amount }
       total_expenses = week.second.count
       average = amount/total_expenses
